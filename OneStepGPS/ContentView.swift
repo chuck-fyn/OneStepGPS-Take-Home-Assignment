885ef38d48edca90721c05e58f7008b0d10aa7b1
Charlie Prutting <cprutting@gmail.com>
2025-04-21 12:54:35 -0700

    Initial Commit

---
+++ OneStepGPS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneStepGPS
+//
+//  Created by Charles Prutting on 4/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OneStepGPS/OneStepGPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneStepGPSApp.swift
+//  OneStepGPS
+//
+//  Created by Charles Prutting on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct OneStepGPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
